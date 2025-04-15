cf30523c96d989d1c263ecc65f2f390b0f23f0f2
Olaiya Olayimika <64112251+yinks002@users.noreply.github.com>
2025-04-15 17:02:16 +0100

    Add files via upload

--- context/index.js
+++ context/index.js
@@ -18,29 +18,112 @@ export const StateContextProvider = ({children}) =>{
     const {mutateAsync: listProperty, isLoading, error} = useContractWrite(
         contract, "listProperty"
     );
-    const createPropertyFunction = async()=>{
+    const createPropertyFunction = async(form)=>{
+
+        const {propertyTitle,
+            description,
+            category,
+            price,
+            images,
+            propertyAddress
+        } = form;
         try {
-            const data= await listProperty([
-                 
+            const data= await listProperty({
+                 args:[
                     address,
                     price,
-                    _propertyTitle,
-                    _category,
-                    _images,
-                    _propertyAddress,
-                    _description,
+                    propertyTitle,
+                    category,
+                    images ,
+                    propertyAddress,
+                    description,
+
+                 ],
 
                 
-            ]);
+        });
             console.info("contract call successfull", data)
         } catch (error) {
             console.log("contract call failed", error)
         }
     }
 
+//update property function
+const {mutateAsync: updateProperty, isLoading: updatePropertyLoading} = useContractWrite(
+    contract, "updateProperty"
+);
+    const updatePropertyFunction = async(form) => {
+        const {productId, propertyTitle,
+            description,
+            category,
+            images,
+            propertyAddress
+        } = form;
+
+        try {
+            const data= await updateProperty({
+                args: [
+                    address,productId, propertyTitle,
+                        description,
+                        category,
+                        images,
+                        propertyAddress 
+                ]
+            });
+            console.log("contract call successfull", data);
+
+        } catch (error) {
+            console.log("errot while updating", error)
+        }
+
+    
+    }
+
+//update price function
+const {mutateAsync: updatePrice, isLoading: updatePriceLoading} = useContractWrite(
+    contract, "updatePrice"
+);
+    const updatePriceFunction = async(form)=>{
+        const {productId,price} = form;
+        try {
+            const data=await updatePrice([address, productId, price])
+
+            console.log("transaction successful", data)
+        } catch (error) {
+            console.log("transaction fail",error)
+        }
+    }
+
+
+    //get properties data section
+    const getPropertiesData= async()=>{
+        try {
+           const properties = await contract.call("getAllProperties");
+            const parsedProperties = properties.map((property, i)=> ({
+                owner: property.owner,
+                title: property.title,
+                description: property.description,
+                category: property.category,
+                price: ethers.utils.formatEther(property.price.toString()),
+                productId: property.productID.toNumber(),
+                reviewers: property.reviews,
+                image: property.images,
+                address: property.propertyAddress
+            }));
+            console.log(properties)
+            return parsedProperties;
+           
+        } catch (error) {
+            console.log("error " , error)
+        }
+    }
+
     return (
         <StateContext.Provider value={{address , connect
-        , contract, realEstate, createPropertyFunction}}>
+        , contract, realEstate, 
+        createPropertyFunction, 
+        getPropertiesData, updatePropertyFunction
+        ,updatePriceFunction}}>
             {children}
         </StateContext.Provider>
     )
--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,10 @@ import {checkIfImage} from "../utils"
 
 
 const index = () => {
-  const {address, contract, connect, realEstate, createPropertyFunction} = useStateContext();
+  const {address, 
+    contract, 
+    connect, realEstate, 
+    createPropertyFunction, getPropertiesData} = useStateContext();
 
   //state variable
   const [isLoading, setIsLoading]  = useState(false);
@@ -41,17 +44,71 @@ const index = () => {
       }
     })
   }
+  const fetchProperty = async()=>{
+    setIsLoading(true);
+    const data = await getPropertiesData();
+    setProperties(data);
+    isLoading(false);
 
-  return <div>
+  };
+  useEffect(()=>{
+    if(contract) fetchProperty();
+    
+    
+  },[address, contract])
+
+  return( <div>
     <h1> {realEstate} </h1>
     <button onClick = {()=> connect()}> Connect </button>
 
     <h1>create</h1>
     <form onSubmit= {handleSubmit }>
-    <div></div>
+    <div>
+      <input type="text" placeholder="propertyTitle" onChange={(e)=> 
+        handleFormFieldChange("propertyTitle", e)
+      } />
+    </div>
+
+    <div>
+      <input type="text" placeholder="description" onChange={(e)=> 
+        handleFormFieldChange("description", e)
+      } />
+    </div>
+
+
+    <div>
+      <input type="text" placeholder="category" onChange={(e)=> 
+        handleFormFieldChange("category", e)
+      } />
+    </div>
+
+
+    <div>
+      <input type="number" placeholder="price" onChange={(e)=> 
+        handleFormFieldChange("price", e)
+      } />
+    </div>
+
+
+    <div>
+      <input type="url" placeholder="images" onChange={(e)=> 
+        handleFormFieldChange("images", e)
+      } />
+    </div>
+
+
+    <div>
+      <input type="text" placeholder="propertyAddress" onChange={(e)=> 
+        handleFormFieldChange("propertyAddress", e)
+      } />
+    </div>
+
+    <button type="submit" >Submit</button>
+
 
     </form>
-  </div>;
+  </div>
+  );
 };
 
 export default index;
