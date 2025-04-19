dc5096e9297ea74168008034d06bd666f84f4320
Olaiya Olayimika <64112251+yinks002@users.noreply.github.com>
2025-04-19 13:06:14 +0100

    Add files via upload

--- context/index.js
+++ context/index.js
@@ -12,24 +12,11 @@ const StateContext = createContext();
 export const StateContextProvider = ({children}) =>{
 
     
-// const client = createThirdwebClient({
-//     // use clientId for client side usage
-//     clientId: "9c3aeddf2491d88c58b283f931a63aa1",
-//     // use secretKey for server side usage
-//     secretKey: "nuo...wmzg", // replace this with full secret key
-//   });
-    // const client = createThirdwebClient({
-    // clientId: "9c3aeddf2491d88c58b283f931a63aa1",
-    // });
-    //  const contract = getContract({
-    //     client,
-    //     chain: defineChain(11155111),
-    //     address: "0xBC13A93A5EEc494b0fC047323bDe6638C00FC49F",
-    //   });
+
    
     const { contract } = useContract(
         "0xBC13A93A5EEc494b0fC047323bDe6638C00FC49F", 
-        RealEstateABI.abi // The pasted ABI array
+        RealEstateABI.abi 
       );
       
         
@@ -121,7 +108,58 @@ const {mutateAsync: updatePrice, isLoading: updatePriceLoading} = useContractWri
     }
 
 
-    //get properties data section
+//buy property
+    const {mutateAsync: buyProperty, isLoading: buyPropertyLoading} = useContractWrite(
+    contract, "buyProperty"
+    );
+    const buyPropertyFunction = async (form) =>{
+        const {id} = form;
+        try {
+            const data = await buyProperty({args: [id, address]})
+            console.log("bought property successfully", data);
+        } catch (error) {
+            console.log("buying propery failed", error);
+        }
+    }
+    
+
+
+    //review section
+    //add review
+    const {mutateAsync: addReview, isLoading: addReviewLoading} = useContractWrite(
+        contract, "addReview"
+        );
+        const addReviewFunction  = async(form)=>{
+          const {productId, rating, comment}  = form;
+          try {
+            const data =await addReview({
+                args: [productId, rating, comment, address],
+            });
+            console.log("review successfully added", data);
+          } catch (error) {
+            console.log("error adding review", error);
+          }
+        }
+
+        //like review
+        const {mutateAsync: likeReview, isLoading: likeReviewLoading} = useContractWrite(
+            contract, "likeReview"
+            );
+
+            const likeReviewFunction = async(form)=>{
+                const {productId, reviewIndex} = form;
+                try {
+                    const data = await likeReview({
+                        args: [productId, reviewIndex]
+                    })
+                    console.log(
+                        "successfully liked review", data
+                    )
+                } catch (error) {
+                    console.log("failed to like", error)
+                }
+            }
+            //get properties data section
     const getPropertiesData= async()=>{
         try {
            const properties = await contract.call("getAllProperties");
@@ -145,6 +183,103 @@ const {mutateAsync: updatePrice, isLoading: updatePriceLoading} = useContractWri
         }
     }
 
+    //getHighestratedProduct function
+    const {data: getHighestratedProduct, isLoading: getHighestratedProductLoading}= useContractRead(
+        contract, "getHighestratedProduct"
+    );
+
+    //getProductReviews function
+        const getProductReviewsFunction = (productId) =>{
+            try {
+                const {data:getProductReviews, isLoading: getProductReviewsLoading} = useContract(
+                     "getProductReviews", [productId]
+                );
+                return (getProductReviews, getProductReviewsLoading)
+
+            } catch (error) {
+                console.log("error fetching data", error)
+            }
+        }
+
+      //get property function
+      
+      const getPropertyFunction = (id)=>{
+        try {
+            const {data: getProperty, isLoading: getPropertyLoading} = useContractRead(
+                 'getProperty', [id]
+            );
+            return (getProperty, getPropertyLoading)
+        } catch (error) {
+            console.log('error while getting property data', error)
+        }
+      }
+
+      //get user property function
+      const getUserPropertiesFunction = ()=>{
+        try {
+             const {data: getUserProperties, isLoading: getUserPropertiesLoading} = 
+             useContractRead("getUserProperties",[address]);
+             return getUserProperties, getUserPropertiesLoading
+        } catch (error) {
+            console.log("error while getting user property", error)
+        }
+      }
+
+      //get user review function
+      const getUserReviewFunction= ()=>{
+          try {
+            const {data: getUserReviews, isLoading: getUserReviewsLoading} = useContractRead(
+                "getUserReviews", [address]
+            );
+            return getUserReviews, getUserReviewsLoading;
+
+          } catch (error) {
+            console.log("error getting user reviews", error)
+          }
+      }
+
+      //total property function
+      const totalPropertyFunction = ()=>{
+        try {
+            const {data: totalProperty, isLoading: totalPropertyLoading} = useContractRead(
+                contract, 'propertyIndex' 
+            )
+            return totalProperty, totalPropertyLoading;
+        } catch (error) {
+            console.log("error fetching total property", error)
+        }
+      }
+
+      //total reviews
+      const totalReviewFunction =()=>{
+        try {
+            const {data: totalReviewsCount, isLoading: totalReviewsCountLoading} = useContractRead(
+                contract, 'reviewsCounter'
+            );
+            return( totalReviewsCount, totalReviewsCountLoading);
+
+        } catch (error) {
+            console.log("error", error)
+        }
+      }
+
+      //read events data
+      const {data: event} = useContractEvents(
+        contract, "propertyListed"
+      );
+
+      //get all events
+      const {data: allEvents} = useContractEvents(contract);
+      //set default
+      const  {data: eventWithoutListener}= useContractEvents(contract
+        , undefined,{
+            subscribe: false,
+        }
+      )
+      console.log("events", event)
+
+
+
     useEffect(()=>{
         const fetchCont= async()=>{
             const cont= await contract;
@@ -161,8 +296,17 @@ const {mutateAsync: updatePrice, isLoading: updatePriceLoading} = useContractWri
         <StateContext.Provider value={{address , connect
         , contract, realEstate, 
         createPropertyFunction, 
-        getPropertiesData, updatePropertyFunction
-        ,updatePriceFunction}}>
+        getPropertiesData, 
+        updatePropertyFunction, 
+        getUserPropertiesFunction, buyPropertyFunction
+        ,updatePriceFunction, 
+        getPropertyFunction,
+        totalPropertyFunction,
+        getUserReviewFunction,
+        totalReviewFunction,
+        addReviewFunction,
+        getProductReviewsFunction,
+        likeReviewFunction}}>
             {children}
         </StateContext.Provider>
     )
